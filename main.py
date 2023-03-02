8bacc746dc836083b2a443c368e03a4291a8b8d9
Johniece Clarke <>
2023-03-02 03:16:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("WELCOME TO THE BAND GENERATOR!")
+city = input("What city did you grow up in?\n ") 
+pet = input("What is your first pets name?\n ")
+print("Your band name could be" + " " + city  + " " + pet)
+
